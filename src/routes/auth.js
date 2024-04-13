20682febb95d568f40a4cf98656958b0bed245aa
Duonganhdu2002 <Wydanhdu@gmail.com>
2024-04-13 15:57:39 +0700

    add code login and register

---
+++ src/routes/auth.js
@@ -0,0 +1,57 @@
+// routes/auth.js
+const express = require("express");
+const router = express.Router();
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const User = require("../models/User");
+const Token = require("../models/Token");
+
+
+// Registration
+router.post("/register", async (req, res) => {
+  try {
+    const hashedPassword = await bcrypt.hash(req.body.password, 10);
+    const user = new User({
+      username: req.body.username,
+      password: hashedPassword,
+    });
+    await user.save();
+    res.status(201).send("User registered successfully");
+  } catch (error) {
+    res.status(500).send(error.message);
+  }
+});
+
+// Login
+router.post("/login", async (req, res) => {
+  try {
+    const user = await User.findOne({ username: req.body.username });
+    if (!user) {
+      return res.status(401).send("Invalid username or password");
+    }
+    const validPassword = await bcrypt.compare(
+      req.body.password,
+      user.password
+    );
+    if (!validPassword) {
+      return res.status(401).send("Invalid username or password");
+    }
+    const token = jwt.sign({ userId: user._id, username: user.username }, "secretkey", {
+      expiresIn: "1h",
+    });
+
+    // Save the token in the Tokens collection
+    const tokenDocument = new Token({
+      userId: user._id,
+      token: token,
+      expiresIn : new Date(Date.now())
+    });
+    await tokenDocument.save();
+
+    res.status(200).json({ token });
+  } catch (error) {
+    res.status(500).send(error.message);
+  }
+});
+
+module.exports = router;
--- src/server.js
+++ src/server.js
@@ -1,22 +1,35 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const cron = require("node-cron");
 require("dotenv").config();
 
 const app = express();
-app.use(express.json())
+app.use(express.json());
 const port = process.env.PORT || 3000;
 
-const exampleRouter = require("./routes/exampleRouter");
-app.use("/", exampleRouter);
+const tokenUtils = require("./untils/tokenUtils");
+const authRoutes = require("./routes/auth");
+const authenticate = require("./middleware/authenticate");
 
-// Middleware to handle invalid paths
+// Middleware để xóa token hết hạn mỗi phút
+cron.schedule("* * * * *", () => {
+  tokenUtils.deleteExpiredTokens();
+});
+
+// Route handlers
+app.use("/auth", authRoutes);
+
+app.get("/protected", authenticate, (req, res) => {
+  res.send("This is a protected route");
+});
+
+// Middleware xử lý lỗi không xác định và lỗi đường dẫn không hợp lệ
 app.use((req, res, next) => {
   const err = new Error("Invalid path");
   err.status = 404;
   next(err);
 });
 
-// Middleware to handle internal errors
 app.use((err, req, res, next) => {
   res.status(err.status || 500);
   res.send({
@@ -28,6 +41,7 @@ app.use((err, req, res, next) => {
   res.status(500).send("Internal server error!");
 });
 
+// Kết nối cơ sở dữ liệu và khởi động server
 mongoose
   .connect(
     "mongodb+srv://2154810104:O1hjKUouTN2XHeiO@wydanhdu.ilbkii2.mongodb.net/travel_app?retryWrites=true&w=majority&appName=wydanhdu"
