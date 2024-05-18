fb92151ab363747915dd86577358a647af16516d
Duonganhdu2002 <Wydanhdu@gmail.com>
2024-05-18 15:09:41 +0700

    add model

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -8,11 +8,15 @@ const userSchema = new mongoose.Schema({
   email: { type: String, required: true },
   password: { type: String, required: true },
   phone: String,
-  role_id: [{ type: mongoose.Schema.Types.ObjectId, ref: "Role" }],
   waiting_list: [{ type: mongoose.Schema.Types.ObjectId, ref: "Planning" }],
   list_friend: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
   book_mark_list: [{ type: mongoose.Schema.Types.ObjectId, ref: "Place" }],
-  Role: [{ type: mongoose.Schema.Types.ObjectId, ref: "Role" }],
+  roles: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Role",
+    },
+  ],
 });
 
 const User = mongoose.model("User", userSchema);
