0c0a42e634aabb9c6488e58987c2168cee87bea6
Duonganhdu2002 <Wydanhdu@gmail.com>
2024-05-29 16:36:36 +0700

    ...

--- src/server.js
+++ src/server.js
@@ -27,7 +27,7 @@ const port = process.env.PORT || 8080;
 
 mongoose
   .connect(
-    "mongodb+srv://2154810104:O1hjKUouTN2XHeiO@wydanhdu.ilbkii2.mongodb.net/travel_app?retryWrites=true&w=majority&appName=wydanhdu",
+    process.env.MONGO_URI,
     {
       useNewUrlParser: true,
       useUnifiedTopology: true,
@@ -43,92 +43,17 @@ mongoose
       },
     });
 
+    // WebSocket logic
     io.on("connection", (socket) => {
-      console.log("A user connected");
+      console.log("a user connected");
 
-      socket.on("join", (userId) => {
-        socket.join(userId);
-        console.log(`User with ID ${userId} joined room ${userId}`);
-      });
-
-      socket.on("send_friend_request", async (data) => {
-        const { senderId, receiverId } = data;
-
-        try {
-          const sender = await User.findById(senderId);
-          const receiver = await User.findById(receiverId);
-
-          if (sender && receiver) {
-            receiver.waiting_list.push(sender._id);
-            sender.invite_list.push(receiver._id);
-
-            await receiver.save();
-            await sender.save();
-
-            io.to(receiverId).emit("receive_friend_request", {
-              senderId: sender._id,
-              senderUsername: sender.username,
-            });
-          }
-        } catch (error) {
-          console.error("Error sending friend request:", error);
-          socket.emit('friend_request_failed', { error: 'Error sending friend request' });
-        }
-      });
-
-      socket.on("accept_friend_request", async (data) => {
-        const { senderId, receiverId } = data;
-
-        try {
-          const sender = await User.findById(senderId);
-          const receiver = await User.findById(receiverId);
-
-          if (sender && receiver) {
-            sender.list_friend.push(receiver._id);
-            receiver.list_friend.push(sender._id);
-
-            receiver.waiting_list = receiver.waiting_list.filter(
-              (id) => id.toString() !== sender._id.toString()
-            );
-            sender.invite_list = sender.invite_list.filter(
-              (id) => id.toString() !== receiver._id.toString()
-            );
-
-            await sender.save();
-            await receiver.save();
-
-            io.to(senderId).emit("friend_request_accepted", { receiverId });
-          }
-        } catch (error) {
-          console.error("Error accepting friend request:", error);
-        }
-      });
-
-      socket.on("reject_friend_request", async (data) => {
-        const { senderId, receiverId } = data;
-
-        try {
-          const sender = await User.findById(senderId);
-          const receiver = await User.findById(receiverId);
-
-          if (sender && receiver) {
-            receiver.waiting_list = receiver.waiting_list.filter(
-              (id) => id.toString() !== sender._id.toString()
-            );
-            sender.invite_list = sender.invite_list.filter(
-              (id) => id.toString() !== receiver._id.toString()
-            );
-
-            await sender.save();
-            await receiver.save();
-          }
-        } catch (error) {
-          console.error("Error rejecting friend request:", error);
-        }
+      socket.on("send_friend_request", ({ senderId, receiverId, username }) => {
+        // Emit event to the receiver
+        io.to(receiverId).emit("receive_friend_request", { senderId, username });
       });
 
       socket.on("disconnect", () => {
-        console.log("A user disconnected");
+        console.log("user disconnected");
       });
     });
 
