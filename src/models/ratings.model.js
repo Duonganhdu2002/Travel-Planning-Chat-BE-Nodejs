2cbed6bafe2fb649dadf2828de906fd0e93601f2
Duonganhdu2002 <Wydanhdu@gmail.com>
2024-06-03 16:13:20 +0700

    ...

--- src/models/ratings.model.js
+++ src/models/ratings.model.js
@@ -2,8 +2,8 @@ const mongoose = require('mongoose');
 
 const ratingSchema = new mongoose.Schema({
   user_id: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
-  rating: Number,
-  comment: String,
+  rating: { type: Number, required: true },
+  comment: { type: String, required: true },
   place_id: { type: mongoose.Schema.Types.ObjectId, ref: 'Place' }
 });
 
--- src/routes/rating.route.js
+++ src/routes/rating.route.js
@@ -1,13 +1,23 @@
-const controller = require("../controllers/rating.controller");
+const controller = require('../controllers/rating.controller');
 
 module.exports = function (app) {
   app.use(function (req, res, next) {
     res.header(
-      "Access-Control-Allow-Headers",
-      "x-access-token, Origin, Content-Type, Accept"
+      'Access-Control-Allow-Headers',
+      'x-access-token, Origin, Content-Type, Accept'
     );
     next();
   });
 
-  app.get("/api/all-rating", controller.getAllRatings);
+  // Route to get all ratings
+  app.get('/api/all-rating', controller.getAllRatings);
+
+  // Route to create a new rating
+  app.post('/api/rating', controller.createRating);
+
+  // Route to update a rating
+  app.put('/api/rating/:id', controller.updateRating);
+
+  // Route to delete a rating
+  app.delete('/api/rating/:id', controller.deleteRating);
 };
