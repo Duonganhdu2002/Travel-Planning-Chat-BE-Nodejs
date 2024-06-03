6b6431db4c05bdf24e733b4a2835b25346eacaca
nav0114 <vunhatanh9597@gmail.com>
2024-06-03 15:51:43 +0700

    sắp xếp địa điểm từ cao đến thấp theo ratting

--- src/controllers/rating.controller.js
+++ src/controllers/rating.controller.js
@@ -3,7 +3,7 @@ const Rating = require('../models/ratings.model');
 exports.getAllRatings = async (req, res) => {
   try {
     const ratings = await Rating.find({});
-    res.json(ratings);
+    res.status(200).json(ratings);
   } catch (error) {
     res.status(500).json({ error: 'Internal server error' });
   }
